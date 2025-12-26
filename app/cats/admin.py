696e8d7fe0f9bd3eee52bcc0ce30949f4fbd0e5d
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2025-12-26 17:02:37 +0200

    django admin

--- app/cats/admin.py
+++ app/cats/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SpyCat
 
-# Register your models here.
+@admin.register(SpyCat)
+class SpyCatAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'breed', 'years_of_experience', 'salary', 'created_at']
+    search_fields = ['name', 'breed']
+    list_filter = ['breed', 'created_at']
--- app/missions/admin.py
+++ app/missions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Mission, Target
 
-# Register your models here.
+class TargetInline(admin.TabularInline):
+    model = Target
+    extra = 1
+
+@admin.register(Mission)
+class MissionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cat', 'is_complete', 'created_at']
+    list_filter = ['is_complete', 'created_at']
+    inlines = [TargetInline]
+
+@admin.register(Target)
+class TargetAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'country', 'mission', 'is_complete']
+    list_filter = ['is_complete', 'country']
+    search_fields = ['name', 'country']
