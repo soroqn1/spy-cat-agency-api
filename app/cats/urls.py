bb94d2ca5ab7f97844afd0311dabe08dcad9957e
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2025-12-26 17:51:22 +0200

    Fix SpyCat router basename and adjust serializer class logic

--- app/cats/urls.py
+++ app/cats/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import SpyCatViewSet
 
 router = DefaultRouter()
-router.register(r'', SpyCatViewSet, basename='cat')
+router.register(r'', SpyCatViewSet, basename='spycat')
 
 urlpatterns = [
     path('', include(router.urls)),
--- app/cats/views.py
+++ app/cats/views.py
@@ -2,11 +2,13 @@ from rest_framework import viewsets
 from .models import SpyCat
 from .serializers import SpyCatSerializer, SpyCatUpdateSerializer
 
+
 class SpyCatViewSet(viewsets.ModelViewSet):
     queryset = SpyCat.objects.all()
     serializer_class = SpyCatSerializer
-
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    
     def get_serializer_class(self):
-        if self.action in ['update', 'partial_update']:
+        if self.action == 'partial_update':
             return SpyCatUpdateSerializer
-        return super().get_serializer_class()
+        return SpyCatSerializer
